fca6cb472459c1641a68abb5a9173379f6d0dc6f
Khussal Pradhan <pradhankhu@gmail.com>
2022-03-04 08:35:47 +0530

    test2

--- library.js
+++ library.js
@@ -1,2 +1,24 @@
 // Write your code here!
-console.log('Hello');
+const users=["UserA","UserB","UserC","UserD"];
+const loginfield=document.getElementById("logged-user");
+const table = document.getElementById("info-table")
+
+function changeLoggedInUser(){
+    const username=loginfield.value;
+    let message=document.getElementById("logged-in-user-name");
+    if (users.includes(username)){
+        message.innerHTML="Logged in user: " +username;
+        library(username);
+    }
+    else if (!users.includes(username) && username!==""){
+        message.innerHTML="";
+    }
+    else if (username=""){
+        //foobar
+    }
+}
+
+function library(username){
+    
+
+}
