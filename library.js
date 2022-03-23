f1c6de4e4e4be8650ac5a3b990a65ac8201fdc39
spandan1460 <1929250@kiit.ac.in>
2022-03-23 23:52:22 +0530

    Fixed the actions part

--- library.js
+++ library.js
@@ -1,185 +1,154 @@
-// Write your code here!
+//Write your code here.
 
-//Creating the API database of books.
+// List of all users in the Library System.
+const users = ['UserA','UserB','UserC','UserD'];
+
+
+
+// The API data of book list objects. 
 const books = [
     {
         id: 1,
-        Title:"Book1",
-        Author: "Author1",
-        Lender: "UserC",
-        Borrower:"UserB",
+        title:"Book1",
+        author: "Author1",
+        lender: "UserC",
+        borrower:"UserB",
     },
     {
-        id: 2,
-        Title:"Book2",
-        Author: "Author2",
-        Lender: "UserC",
-        Borrower:"-",
+      id: 2,
+      title:"Book2",
+      author: "Author2",
+      lender: "UserC",
+      borrower:"-",
     },
     {
         id: 3,
-        Title:"Book3",
-        Author:"Author3",
-        Lender:"UserD",
-        Borrower:"UserC",
+        title:"Book3",
+        author:"Author3",
+        lender:"UserD",
+        borrower:"UserC",
     },
     {
         id: 4,
-        Title:"Book4",
-        Author:"Author4",
-        Lender:"UserA",
-        Borrower:"-",
+        title:"Book4",
+        author:"Author4",
+        lender:"UserA",
+        borrower:"-",
     },
     {
         id: 5,
-        Title:"Book5",
-        Author:"Author5",
-        Lender:"UserA",
-        Borrower:"-",
+        title:"Book5",
+        author:"Author5",
+        lender:"UserA",
+        borrower:"-",
     },
     {
         id: 6,
-        Title:"Book6",
-        Author:"Author6",
-        Lender:"UserB",
-        Borrower:"UserA",
+        title:"Book6",
+        author:"Author6",
+        lender:"UserB",
+        borrower:"UserA",
     },
 ];
 
-//List of all users in the library system.
-const User = ['UserA','UserB','UserC','UserD'];
 
+// Selecting the display name paragraph Element.
+const displayUser = document.getElementById("logged-in-user-name");
 
-
-//This is the heading of the USER to be displayed whoever is logged in.
-let disp = document.getElementById('logged-in-user-name');
-disp.innerHTML = `No User logged in`;
-
-//Stores the current user logged in.
+//Declaring the default as no users logged in.
+displayUser.innerHTML = `No Users Logged In`;
 let current_user = "";
 
-function display_table(){
 
-    let tableData="";
-books.forEach((book) => {
-    tableData += `<tr>
-    <td>
-        ${book.id}
-    </td>
-    <td>
-        ${book.Title}
-    </td>
-    <td>
-        ${book.Author}
-    </td>
-    <td>
-        ${book.Lender}
-    </td>
-    <td>
-        ${book.Borrower}
-    </td>
-    <td>
-        ${
-            book.Lender === current_user
-            ? "-" : book.Borrower === current_user
-            ? "<button id='returnBook' onClick='returnBook(this)'>Return</button>"
-            : current_user && book.Borrower === "-"
-            ? "<button id='borrow' onClick='borrow(this)'>Borrow</button>"
-            : "-"
-        }
-    </td>
-</tr>`;
-});
-
-if(current_user) {
-    document.getElementById('info-table').innerHTML += `<tr>
-    <td>${books.length + 1}</td>
-    <td><input type="text" id="title" placeholder="Title"></td>
-    <td><input type="text" id="author" placeholder="Author"></td>
-    <td>${current_user}</td>
-    <td>-</td>
-    <td><button id="addBook" onClick="addBook()">Add Book</button></td>
-</tr>`;
-}
-
-/* Adding Attributes of class and id to the table head*/
-const table_head = document.querySelector("thead");
-table_head.setAttribute("id", "info-table-head");
-table_head.setAttribute("class", "book-table-head");
-
-/*Creating a table body tag and adding class and id attributes to it*/
-const table_body = document.createElement("tbody");
-table_body.setAttribute("id", "info-table-body");
-table_body.setAttribute("class","book-table-body");
-document.getElementById("info-table-head").after(table_body); //Placing the tbody tag after the table head in the DOM.
-
-/*Displaying the API database in the table.*/
-document.getElementById("info-table-body").innerHTML = tableData;
-}
-
-
+//Function for rendering the table
+function display_table (){
+    const infoTable = document.getElementById("info-table");
+    infoTable.innerHTML = `<thead>
+    <tr>
+    <th>Id</th>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Lender</th>
+        <th>Borrower</th>
+        <th>Action</th>
+    </tr>
+    </thead>`;
+  books.map((book) => {
+    infoTable.innerHTML += `<tr>
+    <td>${book.id}</td>
+              <td>${book.title}</td>
+              <td>${book.author}</td>
+              <td>${book.lender}</td>
+              <td>${book.borrower}</td>
+              <td>${
+                book.lender === current_user
+                  ? "-"
+                  : book.borrower === current_user
+                  ? "<button id='return_book' onclick='return_book(this)'>Return</button>"
+                  : current_user && book.borrower === "-"
+                  ? "<button id='borrow' onclick='borrow(this)'>borrow</button>"
+                  : "-"
+              }</td>
+              </tr>`;
+  });
+  if (current_user) {
+    infoTable.innerHTML += `<tr>
+  <td>${books.length + 1}</td>
+            <td><input id='title' type='text' placeholder='title'></td>
+            <td><input id='author' type='text' placeholder='Author'></td>
+            <td>${current_user}</td>
+            <td>-</td>
+            <td><button id='borrow' onclick='addBook()'>Add book</button></td>
+            </tr>`;
+  }
+};
 display_table();
 
-function changeLoggedInUser(){
-    let input_user = document.getElementById('logged-user').value;
-    current_user = input_user;
-    let user_present = false;
-    User.map((users) => {
-        if(input_user===users){
-            user_present = true;
-            disp.innerHTML = `Logged in user: ${input_user}`;
-        }
-        // else if(input_present===" "){
-        //     user_present="blank";
-        // }
-    });
-    if(user_present===false){
-        alert("No such users found");
-        disp.innerHTML = `No User logged in`;
-    }
-    // else if(user_present==="blank"){
-    //     alert("User not passed");
-    //     disp.innerHTML = `No User logged in`;
-    // }
-
-    console.log(input_user);
-    console.log("Button was Clicked");
-}
-
-
-function borrow(node) {
-    const bookId = Number(node.parentNode.parentNode.childNodes[0].innerText);
-    const bookIndex = books.findIndex((book) => book.id === bookId);
-    books[bookIndex].borrower = input_user;
-
+// function to perform the onclick action to be performed after user clicks the login button.
+function changeLoggedInUser()  {
+  const user = document.querySelector("#logged-user").value;
+  current_user = user;
+  if (users.includes(user)) {
+    displayUser.innerText = `Logged In User: ${user}`;
     display_table();
-}
+  } else {
+    alert("User not found");
+  }
+};
 
+// Function for borrowing the book
+function borrowBook(node) {
+  const bookId = Number(node.parentNode.parentNode.childNodes[0].innerText);
+  const bookIndex = books.findIndex((book) => book.id === bookId);
+  books[bookIndex].borrower = current_user;
 
-function returnBook(node) {
-    const bookId = Number(node.parentNode.parentNode.childNodes[0].innerText);
-    const bookIndex = books.findIndex((book) => book.id === bookId);
-    books[bookIndex].borrower = "-";
-    display_table();
-}
+  display_table();
+};
 
-function addBook() {
-    const title = document.getElementById('title').value;
-    const author = document.getElementById('author').value;
-    const book = {
-        id: books.length + 1,
-        Title: title,
-        Author: author,
-        Lender: current_user,
-        Borrower: "-",
-    };
-    if(Title && Author) {
-        books.push(book);
-        display_table();
-    }
-    else
-    {
-        alert("Please enter all the fields of the book");
-    }
-    display_table();
-}
+// Function of return_book action by the loggedin user
+function returnBook(node)  {
+  const bookId = Number(node.parentNode.parentNode.childNodes[0].innerText);
+  const bookIndex = books.findIndex((book) => book.id === bookId);
+  books[bookIndex].borrower = "-";
+  display_table();
+};
+
+// Function to addBook by the loggedin user
+function addBook()  {
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const book = {
+    id: books.length + 1,
+    title: title,
+    author: author,
+    lender: current_user,
+    borrower: "-",
+  };
+  if (title && author) {
+    books.push(book);
+  } else {
+    alert("Fill all the fields");
+  }
+  display_table();
+};
+
